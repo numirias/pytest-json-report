2f45527fcd469267297880e736c7ddb12b8ce5a3
numirias <numirias@users.noreply.github.com>
2019-01-21 23:55:28 +0100

    Refactor plugin

--- pytest_jsonreport/plugin.py
+++ pytest_jsonreport/plugin.py
@@ -1,289 +1,221 @@
-from collections import Counter, OrderedDict
+from __future__ import print_function
+from collections import OrderedDict
 from contextlib import contextmanager
 import json
 import logging
 import time
+import warnings
 
 import pytest
 
+from . import serialize
 
-class JSONReport:
-    """The JSON report pytest plugin."""
+
+class JSONReportBase:
 
     def __init__(self, config=None):
-        self.config = config
-        self.start_time = None
-        self.tests = OrderedDict()
-        self.collectors = []
-        self.warnings = []
-        self.report = None
-        self.report_size = 0
-        self.logger = logging.getLogger()
-
-    @property
-    def report_file(self):
-        return self.config.option.json_report_file or \
-               self.config.getini('json_report_file') or \
-               '.report.json'
-
-    @property
-    def want_traceback(self):
-        return not self.config.option.json_report_no_traceback and \
-               self.config.option.tbstyle != 'no'
-
-    @property
-    def want_streams(self):
-        return not self.config.option.json_report_no_streams
-
-    @property
-    def want_logs(self):
-        return not self.config.option.json_report_no_logs
-
-    @property
-    def want_summary(self):
-        return self.config.option.json_report_summary
+        self._config = config
+        self._logger = logging.getLogger()
 
     def pytest_configure(self, config):
         # When the plugin is used directly from code, it may have been
         # initialized without a config.
-        if self.config is None:
-            self.config = config
+        if self._config is None:
+            self._config = config
+        # If the user sets --tb=no, always omit the traceback from the report
+        if self._config.option.tbstyle == 'no' and \
+           not self._must_omit('traceback'):
+            self._config.option.json_report_omit.append('traceback')
 
     def pytest_addhooks(self, pluginmanager):
         pluginmanager.add_hookspecs(Hooks)
 
-    def pytest_sessionstart(self, session):
-        self.start_time = time.time()
-
-    def pytest_collectreport(self, report):
-        collector = self.json_collector(report)
-        if report.longrepr:
-            # Unfortunately, the collection report doesn't provide crash
-            # details, so we can only add the message, but no traceback etc.
-            collector['longrepr'] = str(report.longrepr)
-        self.collectors.append(collector)
-
+    @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_protocol(self, item, nextitem):
-        item._json_log = {}
+        item._json_report_extra = {}
+        yield
+        del item._json_report_extra
 
     @contextmanager
-    def capture_log(self, item, when):
+    def _capture_log(self, item, when):
         handler = LoggingHandler()
-        self.logger.addHandler(handler)
+        self._logger.addHandler(handler)
         try:
             yield
         finally:
-            self.logger.removeHandler(handler)
-        item._json_log[when] = handler.records
+            self._logger.removeHandler(handler)
+        item._json_report_extra[when]['log'] = handler.records
 
     @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_setup(self, item):
-        with self.capture_log(item, 'setup'):
+        item._json_report_extra['setup'] = {}
+        if self._must_omit('log'):
             yield
+        else:
+            with self._capture_log(item, 'setup'):
+                yield
 
     @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_call(self, item):
-        with self.capture_log(item, 'call'):
+        item._json_report_extra['call'] = {}
+        if self._must_omit('log'):
             yield
+        else:
+            with self._capture_log(item, 'call'):
+                yield
 
     @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_teardown(self, item):
-        with self.capture_log(item, 'teardown'):
+        item._json_report_extra['teardown'] = {}
+        if self._must_omit('log'):
             yield
+        else:
+            with self._capture_log(item, 'teardown'):
+                yield
 
     @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_makereport(self, item, call):
-        outcome = yield
-        report = outcome.get_result()
+        # Hook runtest_makereport to access the item *and* the report
+        report = (yield).get_result()
+        if not self._must_omit('streams'):
+            streams = {key: val for when_, key, val in item._report_sections if
+                       when_ == report.when and key in ['stdout', 'stderr']}
+            item._json_report_extra[call.when].update(streams)
+        self._validate_metadata(item)
+        # Attach the JSON details to the report. If this is an xdist worker,
+        # the details will be serialized and relayed with the other attributes
+        # of the report.
+        report._json_report_extra = item._json_report_extra
+
+    @staticmethod
+    def _validate_metadata(item):
+        """Ensure that `item` has JSON-serializable metadata, otherwise delete
+        it."""
+        if 'metadata' not in item._json_report_extra:
+            return
         try:
-            test = self.tests[item]
+            json.dumps(item._json_report_extra['metadata'])
+        except (TypeError, OverflowError):
+            warnings.warn(
+                'Metadata of {} is not JSON-serializable.'.format(item.nodeid))
+            del item._json_report_extra['metadata']
+
+    def _must_omit(self, key):
+        return key in self._config.option.json_report_omit
+
+
+class JSONReport(JSONReportBase):
+    """The JSON report pytest plugin."""
+
+    def __init__(self, *args, **kwargs):
+        JSONReportBase.__init__(self, *args, **kwargs)
+        # Path to save the report to
+        self._report_path = None
+        self._report_size = 0
+        self._start_time = None
+        self._json_tests = OrderedDict()
+        self._json_collectors = []
+        self._json_warnings = []
+        self.report = None
+
+    def pytest_configure(self, config):
+        JSONReportBase.pytest_configure(self, config)
+        self._report_path = self._config.option.json_report_file or \
+            self._config.getini('json_report_file') or \
+            '.report.json'
+
+    def pytest_sessionstart(self, session):
+        self._start_time = time.time()
+
+    def pytest_collectreport(self, report):
+        if not self._must_omit('collectors'):
+            self._json_collectors.append(serialize.make_collector(report))
+
+    def pytest_runtest_logreport(self, report):
+        nodeid = report.nodeid
+        details = report._json_report_extra
+        try:
+            json_testitem = self._json_tests[nodeid]
         except KeyError:
-            test = self.json_testitem(item)
-            self.tests[item] = test
+            json_testitem = serialize.make_testitem(
+                nodeid,
+                # report.keywords is a dict (for legacy reasons), but we just
+                # need the keys
+                None if self._must_omit('keywords') else list(report.keywords),
+                report.location,
+            )
+            self._json_tests[nodeid] = json_testitem
+        metadata = details.get('metadata')
+        if metadata:
+            serialize.testitem_update_metadata(json_testitem, metadata)
         # Update total test outcome, if necessary. The total outcome can be
         # different from the outcome of the setup/call/teardown stage.
-        outcome = self.config.hook.pytest_report_teststatus(report=report)[0]
+        outcome = self._config.hook.pytest_report_teststatus(report=report)[0]
         if outcome not in ['passed', '']:
-            test['outcome'] = outcome
-        test[call.when] = self.json_teststage(item, report)
+            serialize.testitem_update_outcome(json_testitem, outcome)
+        if self._must_omit('traceback'):
+            traceback = None
+        else:
+            try:
+                traceback = report.longrepr.reprtraceback
+            except AttributeError:
+                traceback = None
+        stage_details = details[report.when]
+        json_testitem[report.when] = serialize.make_teststage(
+            report,
+            stage_details.get('stdout'),
+            stage_details.get('stderr'),
+            stage_details.get('log'),
+            traceback,
+        )
 
     def pytest_sessionfinish(self, session):
-        self.add_metadata()
-        json_report = {
-            'created': time.time(),
-            'duration': time.time() - self.start_time,
-            'exitcode': session.exitstatus,
-            'root': str(session.fspath),
-            'environment': getattr(self.config, '_metadata', {}),
-            'summary': self.json_summary(),
-        }
-        if not self.want_summary:
-            json_report['collectors'] = self.collectors
-            json_report['tests'] = list(self.tests.values())
-            if self.warnings:
-                json_report['warnings'] = self.warnings
-        self.config.hook.pytest_json_modifyreport(json_report=json_report)
-        self.save_report(json_report)
-        # self.report isn't ever used, but it's useful if the report needs to
-        # be processed by another script/plugin.
+        json_report = serialize.make_report(
+            created=time.time(),
+            duration=time.time() - self._start_time,
+            exitcode=session.exitstatus,
+            root=str(session.fspath),
+            environment=getattr(self._config, '_metadata', {}),
+            summary=serialize.make_summary(self._json_tests),
+        )
+        if not self._config.option.json_report_summary:
+            if self._json_collectors:  # TODO Write test
+                json_report['collectors'] = self._json_collectors
+            json_report['tests'] = list(self._json_tests.values())
+            # TODO Write test for warnings omission
+            if self._json_warnings and not self._must_omit('warnings'):
+                json_report['warnings'] = self._json_warnings
+
+        self._config.hook.pytest_json_modifyreport(json_report=json_report)
+        self._save_report(json_report)
+        # After the session is finished, other scripts may want to use report
+        # object directly
         self.report = json_report
 
-    def pytest_warning_captured(self, warning_message, when):
-        # warning_message is a warnings.WarningMessage object
-        warning = {
-            'message': str(warning_message.message),
-            'when': when,
-            'filename': warning_message.filename,
-            'lineno': warning_message.lineno
-        }
-        self.warnings.append(warning)
-
-    def pytest_terminal_summary(self, terminalreporter):
-        terminalreporter.write_sep('-', 'JSON report')
-        terminalreporter.write_line('report written to: %s (%d bytes)' %
-                                    (self.report_file, self.report_size))
-
-    def add_metadata(self):
-        """Add metadata from test items to the report."""
-        for item, test in self.tests.items():
-            try:
-                metadata = item._json_metadata
-            except AttributeError:
-                continue
-            if metadata == {}:
-                continue
-            test['metadata'] = metadata
-
-    def save_report(self, json_report):
+    def _save_report(self, json_report):
         """Save the JSON report to file."""
-        with open(self.report_file, 'w') as f:
+        with open(self._report_path, 'w') as f:
             json.dump(
                 json_report,
                 f,
                 default=str,
-                indent=self.config.option.json_report_indent,
+                indent=self._config.option.json_report_indent,
             )
-            self.report_size = f.tell()
+            self._report_size = f.tell()
 
-    def json_collector(self, report):
-        """Return JSON-serializable collector node."""
-        children = []
-        for node in report.result:
-            child = {
-                'nodeid': node.nodeid,
-                'type': node.__class__.__name__,
-            }
-            child.update(self.json_location(node))
-            children.append(child)
+    def pytest_warning_captured(self, warning_message, when):
+        self._json_warnings.append(
+            serialize.make_warning(warning_message, when))
 
-        return {
-            'nodeid': report.nodeid,
-            # This is the outcome of the collection, not the test outcome
-            'outcome': report.outcome,
-            'children': children,
-        }
+    def pytest_terminal_summary(self, terminalreporter):
+        terminalreporter.write_sep('-', 'JSON report')
+        terminalreporter.write_line('report written to: %s (%d bytes)' %
+                                    (self._report_path, self._report_size))
 
-    def json_location(self, node):
-        """Return JSON-serializable node location."""
-        try:
-            path, line, domain = node.location
-        except AttributeError:
-            return {}
-        return {
-            'path': path,
-            'lineno': line,
-            'domain': domain,
-        }
 
-    def json_testitem(self, item):
-        """Return JSON-serializable test item."""
-        obj = {
-            'nodeid': item.nodeid,
-            # item.keywords is actually a dict, but we just save the keys
-            'keywords': list(item.keywords),
-            # The outcome will be overridden in case of failure
-            'outcome': 'passed',
-        }
-        # Adding the location in the collector dict *and* here appears
-        # redundant, but the docs say they may be different
-        obj.update(self.json_location(item))
-        return obj
+class JSONReportWorker(JSONReportBase):
 
-    def json_teststage(self, item, report):
-        """Return JSON-serializable test stage (setup/call/teardown)."""
-        stage = {
-            'duration': report.duration,
-            'outcome': report.outcome,
-        }
-        stage.update(self.json_crash(report))
-        stage.update(self.json_traceback(report))
-        stage.update(self.json_streams(item, report.when))
-        stage.update(self.json_log(item, report.when))
-
-        if report.longreprtext:
-            stage['longrepr'] = report.longreprtext
-        return stage
-
-    def json_streams(self, item, when):
-        """Return JSON-serializable output of the standard streams."""
-        if not self.want_streams:
-            return {}
-        return {key: val for when_, key, val in item._report_sections if
-                when_ == when and key in ['stdout', 'stderr']}
-
-    def json_log(self, item, when):
-        if not self.want_logs:
-            return {}
-        try:
-            return {'log': item._json_log[when]}
-        except KeyError:
-            return {}
-
-    def json_crash(self, report):
-        """Return JSON-serializable crash details."""
-        try:
-            crash = report.longrepr.reprcrash
-        except AttributeError:
-            return {}
-        return {
-            'crash': {
-                'path': crash.path,
-                'lineno': crash.lineno,
-                'message': crash.message,
-            },
-        }
-
-    def json_traceback(self, report):
-        """Return JSON-serializable traceback details."""
-        try:
-            tb = report.longrepr.reprtraceback
-        except AttributeError:
-            return {}
-        if not self.want_traceback:
-            return {}
-        return {
-            'traceback': [{
-                'path': entry.reprfileloc.path,
-                'lineno': entry.reprfileloc.lineno,
-                'message': entry.reprfileloc.message,
-            } for entry in tb.reprentries],
-        }
-
-    def json_summary(self):
-        """Return JSON-serializable test result summary."""
-        summary = Counter([t['outcome'] for t in self.tests.values()])
-        summary['total'] = sum(summary.values())
-        return summary
-
-    @pytest.fixture
-    def json_metadata(self, request):
-        """Fixture to add metadata to the current test item."""
-        try:
-            metadata = request.node._json_metadata
-        except AttributeError:
-            metadata = {}
-            request.node._json_metadata = metadata
-        return metadata
+    pass
 
 
 class LoggingHandler(logging.Handler):
@@ -310,33 +242,41 @@ class Hooks:
         """
 
 
+@pytest.fixture
+def json_metadata(request):
+    """Fixture to add metadata to the current test item."""
+    return request.node._json_report_extra.setdefault('metadata', {})
+
+
 def pytest_addoption(parser):
     file_help_text = 'target path to save JSON report'
-    no_traceback_help_text = 'don\'t include tracebacks in JSON report'
-    no_stream_help_text = 'don\'t include stdout/stderr output in JSON report'
-    no_log_help_text = 'don\'t include log output in JSON report'
-    summary_help_text = 'just create a summary without per-test details'
-    indent_help_text = 'pretty-print JSON with specified indentation level'
     group = parser.getgroup('jsonreport', 'reporting test results as JSON')
-    group.addoption('--json-report', default=False, action='store_true',
-                    help='create JSON report')
+    group.addoption(
+        '--json-report', default=False, action='store_true',
+        help='create JSON report')
     group.addoption('--json-report-file', help=file_help_text)
-    group.addoption('--json-report-no-traceback', default=False,
-                    action='store_true', help=no_traceback_help_text)
-    group.addoption('--json-report-no-streams', default=False,
-                    action='store_true', help=no_stream_help_text)
-    group.addoption('--json-report-summary', default=False,
-                    action='store_true', help=summary_help_text)
-    group.addoption('--json-report-no-logs', default=False,
-                    action='store_true', help=no_log_help_text)
-    group.addoption('--json-report-indent', type=int, help=indent_help_text)
+    group.addoption(
+        '--json-report-omit', default=[], nargs='+', help='list of attributes '
+        'to omit in the report (choose from: collectors, log, traceback, '
+        'streams, warnings, keywords)')
+    group.addoption(
+        '--json-report-summary', default=False,
+        action='store_true', help='only create a summary without per-test '
+        'details')
+    group.addoption(
+        '--json-report-indent', type=int, help='pretty-print JSON with '
+        'specified indentation level')
     parser.addini('json_report_file', file_help_text)
 
 
 def pytest_configure(config):
     if not config.option.json_report:
         return
-    plugin = JSONReport(config)
+    if hasattr(config, 'workerinput'):
+        Plugin = JSONReportWorker
+    else:
+        Plugin = JSONReport
+    plugin = Plugin(config)
     config._json_report = plugin
     config.pluginmanager.register(plugin)
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,190 @@
+import json
+
+import pytest
+
 pytest_plugins = 'pytester'
+miss_map = {
+    'V': 'Different values',
+    'K': 'Different keys',
+    'T': 'Different types',
+}
+# Some test cases borrowed from github.com/mattcl/pytest-json
+FILE = """
+from __future__ import print_function
+import sys
+import pytest
+
+
+@pytest.fixture
+def setup_teardown_fixture(request):
+    print('setup')
+    print('setuperr', file=sys.stderr)
+    def fn():
+        print('teardown')
+        print('teardownerr', file=sys.stderr)
+    request.addfinalizer(fn)
+
+@pytest.fixture
+def fail_setup_fixture(request):
+    assert False
+
+@pytest.fixture
+def fail_teardown_fixture(request):
+    def fn():
+        assert False
+    request.addfinalizer(fn)
+
+
+def test_pass():
+    assert True
+
+def test_fail_with_fixture(setup_teardown_fixture):
+    print('call')
+    print('callerr', file=sys.stderr)
+    assert False
+
+@pytest.mark.xfail(reason='testing xfail')
+def test_xfail():
+    assert False
+
+@pytest.mark.xfail(reason='testing xfail')
+def test_xfail_but_passing():
+    assert True
+
+def test_fail_during_setup(fail_setup_fixture):
+    assert True
+
+def test_fail_during_teardown(fail_teardown_fixture):
+    assert True
+
+@pytest.mark.skipif(True, reason='testing skip')
+def test_skip():
+    assert False
+
+def test_fail_nested():
+    def baz(o=1):
+        c = 3
+        return 2 - c - None
+    def bar(m, n=5):
+        b = 2
+        print(m)
+        print('bar')
+        return baz()
+    def foo():
+        a = 1
+        print('foo')
+        v = [bar(x) for x in range(3)]
+        return v
+    foo()
+
+@pytest.mark.parametrize('x', [1, 2])
+def test_parametrized(x):
+    assert x == 1
+"""
+
+
+@pytest.fixture
+def misc_testdir(testdir):
+    testdir.makepyfile(FILE)
+    return testdir
+
+
+@pytest.fixture
+def json_data(make_json):
+    return make_json()
+
+
+@pytest.fixture
+def tests(json_data):
+    return tests_only(json_data)
+
+
+def tests_only(json_data):
+    return {test['nodeid'].split('::')[-1][5:]: test for test in
+            json_data['tests']}
+
+
+# Each test run should work with and without xdist (-n specifies workers)
+@pytest.fixture(params=[0, 1])
+def num_processes(request):
+    return request.param
+
+
+@pytest.fixture
+def make_json(num_processes, testdir):
+    def func(content=FILE, args=['-vv', '--json-report', '-n=%d' %
+             num_processes], path='.report.json'):
+        testdir.makepyfile(content)
+        testdir.runpytest(*args)
+        with open(str(testdir.tmpdir / path)) as f:
+            data = json.load(f)
+        return data
+    return func
+
+
+@pytest.fixture
+def match_reports():
+    def f(a, b):
+        diffs = list(diff(normalize_report(a), normalize_report(b)))
+        if not diffs:
+            return True
+        for kind, path, a_, b_ in diffs:
+            path_str = '.'.join(path)
+            kind_str = miss_map[kind]
+            if kind == 'V':
+                print(kind_str, path_str)
+                print('\t', a_)
+                print('\t', b_)
+            else:
+                print(kind_str + ':', path_str, a_, b_)
+        return False
+    return f
+
+
+def normalize_report(report):
+    report['created'] = 0
+    report['duration'] = 0
+    # xdist doesn't report successful node collections
+    report['collectors'] = []
+
+    for test in report['tests']:
+        for stage_name in ('setup', 'call', 'teardown'):
+            try:
+                stage = test[stage_name]
+            except KeyError:
+                continue
+            stage['duration'] = 0
+            if 'longrepr' not in stage:
+                stage['longrepr'] = ''
+    return report
+
+
+def diff(a, b, path=None):
+    """Return differences between reports a and b."""
+    if path is None:
+        path = []
+    # We can't compare "longrepr" because they may be different between runs
+    # with and without workers
+    if path and path[-1] != 'longrepr':
+        return
+    if type(a) != type(b):
+        yield ('T', path, a, b)
+        return
+    if type(a) == dict:
+        a_keys = sorted(a.keys())
+        b_keys = sorted(b.keys())
+        if a_keys != b_keys:
+            yield ('K', path, a_keys, b_keys)
+            return
+        for ak, bk in zip(a_keys, b_keys):
+            for item in diff(a[ak], b[bk], path + [str(ak)]):
+                yield item
+        return
+    if type(a) == list:
+        for i, (ai, bi) in enumerate(zip(a, b)):
+            for item in diff(ai, bi, path + [str(i)]):
+                yield item
+        return
+    if a != b:
+        yield ('V', path, repr(a), repr(b))
+    return
