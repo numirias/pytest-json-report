0a9b1ead18fbcd5de2901e70a254c6cde15e948f
numirias <numirias@users.noreply.github.com>
2021-06-18 04:34:32 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     packages=['pytest_jsonreport'],
     author='numirias',
     author_email='numirias@users.noreply.github.com',
-    version='1.3.0',
+    version='1.4.0',
     url='https://github.com/numirias/pytest-json-report',
     license='MIT',
     install_requires=[
