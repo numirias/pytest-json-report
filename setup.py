47503c35a9c43cad89912d5348a9d49239c9c8c3
numirias <numirias@users.noreply.github.com>
2022-03-14 18:44:21 +0100

    Update Python version range

--- setup.py
+++ setup.py
@@ -37,11 +37,10 @@ setup(
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Framework :: Pytest',
     ],
 )
